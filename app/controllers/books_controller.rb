b81a1530eff41ff954a7bd5b5bf8ca11ad2292fe
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-24 19:15:19 -0300

    Actions on Books Controller created - Index and show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,15 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :genre, :cover, :resume, :year, :rating)
+  end
 end
